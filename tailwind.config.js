18802b1efa4ef780395ab3c3054cb7f4772db78d
Evan Jamaq <xevan19@gmail.com>
2024-09-25 07:54:36 +0700

    edit konfigurasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ["./src/**/*.{html,js}"],
-  
+  content: ["./src/**/*.{html,js}"], 
   theme: {
     fontFamily: {
       museo: ['MuseoModerno', 'sans-serif'], // Menambahkan font MuseoModerno
